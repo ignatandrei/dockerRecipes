e41d1464d0c3a14b99700a98cb91a615f8eeec23
ignatandrei <ignatandrei@yahoo.com>
2020-10-18 10:41:40 +0300

    simpler

--- sqlServer/copyAndRunSql/emp.sql
+++ sqlServer/copyAndRunSql/emp.sql
@@ -77,49 +77,49 @@ GO
 
 
 
-DROP TABLE [dbo].[TestAndrei]
-GO
+-- DROP TABLE [dbo].[TestAndrei]
+-- GO
 
-CREATE TABLE [dbo].[TestAndrei](
-	[A2] [char](100) NULL,
-	[A3] [varchar](100) NULL,
-	[A4] [varchar](150) NULL,
-	[A5] [text] NULL,
-	[A6] [nchar](1) NULL,
-	[A7] [nvarchar](1) NULL,
-	[A8] [nvarchar](150) NULL,
-	[A9] [ntext] NULL,
-	[A10] [binary](100) NULL,
-	[A11] [varbinary](1) NULL,
-	[A12] [varbinary](150) NULL,
-	[A13] [image] NULL,
-	[A14] [bit] NULL,
-	[A15] [tinyint] NULL,
-	[A16] [smallint] NULL,
-	[A17] [int] NULL,
-	[A18] [bigint] NULL,
-	[A19] [decimal](10, 2) NULL,
-	[A20] [numeric](10, 4) NULL,
-	[A21] [smallmoney] NULL,
-	[A22] [money] NULL,
-	[A23] [float] NULL,
-	[A24] [real] NULL,
-	[A25] [datetime] NULL,
-	[A26] [datetime2](7) NULL,
-	[A27] [smalldatetime] NULL,
-	[A28] [date] NULL,
-	[A29] [time](7) NULL,
-	[A30] [datetimeoffset](7) NULL,
-	[A31] [timestamp] NOT NULL,
-	[A32] [sql_variant] NULL,
-	[A33] [uniqueidentifier] NULL,
-	[A34] [xml] NULL,
-	[id] [int] IDENTITY(1,1) NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+-- CREATE TABLE [dbo].[TestAndrei](
+	-- [A2] [char](100) NULL,
+	-- [A3] [varchar](100) NULL,
+	-- [A4] [varchar](150) NULL,
+	-- [A5] [text] NULL,
+	-- [A6] [nchar](1) NULL,
+	-- [A7] [nvarchar](1) NULL,
+	-- [A8] [nvarchar](150) NULL,
+	-- [A9] [ntext] NULL,
+	-- [A10] [binary](100) NULL,
+	-- [A11] [varbinary](1) NULL,
+	-- [A12] [varbinary](150) NULL,
+	-- [A13] [image] NULL,
+	-- [A14] [bit] NULL,
+	-- [A15] [tinyint] NULL,
+	-- [A16] [smallint] NULL,
+	-- [A17] [int] NULL,
+	-- [A18] [bigint] NULL,
+	-- [A19] [decimal](10, 2) NULL,
+	-- [A20] [numeric](10, 4) NULL,
+	-- [A21] [smallmoney] NULL,
+	-- [A22] [money] NULL,
+	-- [A23] [float] NULL,
+	-- [A24] [real] NULL,
+	-- [A25] [datetime] NULL,
+	-- [A26] [datetime2](7) NULL,
+	-- [A27] [smalldatetime] NULL,
+	-- [A28] [date] NULL,
+	-- [A29] [time](7) NULL,
+	-- [A30] [datetimeoffset](7) NULL,
+	-- [A31] [timestamp] NOT NULL,
+	-- [A32] [sql_variant] NULL,
+	-- [A33] [uniqueidentifier] NULL,
+	-- [A34] [xml] NULL,
+	-- [id] [int] IDENTITY(1,1) NOT NULL,
+-- PRIMARY KEY CLUSTERED 
+-- (
+	-- [id] ASC
+-- )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+-- ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+-- GO
 
 
